f8cc1e419b1cedc53070a1d4178e3217ddbeb41c
Samuele Marro <marrosamuele@gmail.com>
2021-09-16 11:01:30 +0200

    Removed address info from Header.

--- components/Header.js
+++ components/Header.js
@@ -1,49 +1,37 @@
 import React from "react";
 import { RoutingLink, WalletButton } from ".";
-import PromiseLoader from "./PromiseLoader";
-import { getWalletAddress } from "../common/utils"
-import { useWalletProvider } from "../common/provider";
 
 export default function Header() {
-    const [walletProvider, setWalletProvider] = useWalletProvider()
-
     return (
         <nav className="navbar" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
-            <a className="navbar-item" href=".">
-                <h1 className="title">{".zang{"}</h1>
-            </a>
+                <a className="navbar-item" href=".">
+                    <h1 className="title">{".zang{"}</h1>
+                </a>
 
-            <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
-            </div>
-
-            <div id="navbarBasicExample" className="navbar-menu">
-            <div className="navbar-start">
-                <RoutingLink href='/' className="navbar-item">
-                    Home
-                </RoutingLink>
-
-                <RoutingLink href='/mint' className="navbar-item">
-                    Mint
-                </RoutingLink>
-            </div>
-            
-            
-            <div className="navbar-end">
-                <div className="navbar-item">
-                    {walletProvider ? <></> : <WalletButton />}
+                <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
                 </div>
-            </div>
-                <PromiseLoader promise={() => getWalletAddress(walletProvider)} deps={[walletProvider]}
-                    render={(user) => (
-                        user ? <h4 className="title is-4 has-text-centered">Welcome {user}!</h4> : <h4 className="title is-4 has-text-centered">Not Connected</h4>
-                    )}
-                    loading={<h4 className="title is-4 has-text-centered">Loading...</h4>}
-                    error = {(error) => {console.log(error); return <p>{error.message}</p>}}/>
+
+                <div id="navbarBasicExample" className="navbar-menu">
+                <div className="navbar-start">
+                    <RoutingLink href='/' className="navbar-item">
+                        Home
+                    </RoutingLink>
+
+                    <RoutingLink href='/mint' className="navbar-item">
+                        Mint
+                    </RoutingLink>
+                </div>
+
+                <div className="navbar-end">
+                    <div className="navbar-item">
+                        <WalletButton />
+                    </div>
+                </div>
             </div>
         </nav>
     )
