02f2ae882c52ee94686d81b5ef2b2a97464f86c9
Samuele Marro <marrosamuele@gmail.com>
2021-09-16 17:54:29 +0200

    Fixed Joi validation.

--- common/schemas.js
+++ common/schemas.js
@@ -1,12 +1,18 @@
 import Joi from "joi";
 
+const _customRecipient = Joi.alternatives().try(
+    Joi.string().domain({ tlds: { allow: ['eth'] } }).when('useCustomRecipient', { is: true, then: Joi.required() }), // (ENS) domain
+    Joi.string().pattern(/^0x[a-fA-F0-9]{40}$/).when('useCustomRecipient', { is: true, then: Joi.required() }), // Address
+    Joi.string().when('useCustomRecipient', {is : true, then: Joi.valid('').forbidden(''), otherwise: Joi.string().empty('')}) // Other string (when it is not required)
+)
+
 const mint = Joi.object().keys({
-    title: Joi.string().allow(''),
-    description: Joi.string().allow(''),
+    title: Joi.string().allow('').label('Title'),
+    description: Joi.string().allow('').label('Description'),
     editionSize: Joi.number().precision(0).min(1).empty('').required().label('Edition size'),
     royaltyPercentage: Joi.number().precision(2).min(1).max(100).empty('').required().label('Royalty percentage'),
     useCustomRecipient: Joi.boolean().required(),
-    customRecipient: Joi.string(),
+    customRecipient: _customRecipient.label('Address'),
     textType: Joi.valid('text/plain', 'text/markdown').required()
 })
 
