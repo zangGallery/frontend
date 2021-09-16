25f38be05b7d5980fcca0a767e9ec8eced0ecce5
Samuele Marro <marrosamuele@gmail.com>
2021-09-16 19:28:40 +0200

    More validation fixes.

--- common/schemas.js
+++ common/schemas.js
@@ -1,16 +1,32 @@
 import Joi from "joi";
 
+const _alwaysInvalid = (value, helpers) => {
+    helpers.error('any.only')
+}
+
 const _customRecipient = Joi.alternatives().try(
     Joi.string().domain({ tlds: { allow: ['eth'] } }).when('useCustomRecipient', { is: true, then: Joi.required() }), // (ENS) domain
     Joi.string().pattern(/^0x[a-fA-F0-9]{40}$/).when('useCustomRecipient', { is: true, then: Joi.required() }), // Address
-    Joi.string().when('useCustomRecipient', {is : true, then: Joi.valid('').forbidden(''), otherwise: Joi.string().empty('')}) // Other string (when it is not required)
+    Joi.string().when('useCustomRecipient', {is : true, then: Joi.custom(_alwaysInvalid), otherwise: Joi.string().empty('')}) // Other string (when it is not required)
 )
+
+const maxDigits = (max) => (value, helpers) => {
+    const convertedValue = helpers.original + '';
+    if (convertedValue.includes('.')) {
+        const digitCount = convertedValue.split('.')[1].length;
+        if (digitCount > max) {
+            return helpers.error('number.precision', { limit: max})
+        }
+    }
+
+    return value;
+}
 
 const mint = Joi.object().keys({
     title: Joi.string().allow('').label('Title'),
     description: Joi.string().allow('').label('Description'),
-    editionSize: Joi.number().precision(0).min(1).empty('').required().label('Edition size'),
-    royaltyPercentage: Joi.number().precision(2).min(1).max(100).empty('').required().label('Royalty percentage'),
+    editionSize: Joi.number().integer().min(1).empty('').required().label('Edition size'),
+    royaltyPercentage: Joi.number().custom(maxDigits(2)).positive().max(100).empty('').required().label('Royalty percentage'),
     useCustomRecipient: Joi.boolean().required(),
     customRecipient: _customRecipient.label('Address'),
     textType: Joi.valid('text/plain', 'text/markdown').required()
