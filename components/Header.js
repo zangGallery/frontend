61a090b215e42500d7fcbe04191624cb693d2456
lucadonnoh <donnoh99@gmail.com>
2021-09-19 20:26:29 +0200

    fixed inexistent function

--- components/Header.js
+++ components/Header.js
@@ -2,8 +2,6 @@ import React from "react";
 import { RoutingLink, WalletButton } from ".";
 
 export default function Header() {
-<<<<<<< HEAD
-    const [provider, setProvider] = useProvider()
     const [isActive, setActive] = React.useState(false)
     
     function toggleClass() {
@@ -11,8 +9,6 @@ export default function Header() {
         console.log(isActive);
     }
 
-=======
->>>>>>> ed89cf62212077430cc929a25d84485ce6ac5bfe
     return (
         <nav className="navbar" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
@@ -20,7 +16,6 @@ export default function Header() {
                     <h1 className="title">{".zang{"}</h1>
                 </a>
 
-<<<<<<< HEAD
             <a role="button" className={"navbar-burger" + (isActive ? " is-active" : "")} onClick={toggleClass} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                 <span aria-hidden="true"></span>
                 <span aria-hidden="true"></span>
@@ -33,20 +28,6 @@ export default function Header() {
                 <RoutingLink href='/' className="navbar-item">
                     Home
                 </RoutingLink>
-=======
-                <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
-                </div>
-
-                <div id="navbarBasicExample" className="navbar-menu">
-                <div className="navbar-start">
-                    <RoutingLink href='/' className="navbar-item">
-                        Home
-                    </RoutingLink>
->>>>>>> ed89cf62212077430cc929a25d84485ce6ac5bfe
 
                     <RoutingLink href='/mint' className="navbar-item">
                         Mint
