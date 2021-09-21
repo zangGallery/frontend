50db41a32240cd9185997cffe1538d6d33099fe1
Samuele Marro <marrosamuele@gmail.com>
2021-09-21 17:35:55 +0200

    Added NFT navigation.

--- src/pages/nft.js
+++ src/pages/nft.js
@@ -15,6 +15,18 @@ import { Header } from '../components';
 import "bulma/css/bulma.min.css";
 import '../styles/globals.css'
 
+const styles = {
+    arrowContainer: {
+        display: 'flex',
+        justifyContent: 'flex-end',
+        marginTop: '1em'
+    },
+    arrow: {
+        fontSize: '2em',
+        marginRight: '0.75em'
+    }
+}
+
 export default function NFTPage( { location }) {
     const { id } = queryString.parse(location.search);
     const [tokenURI, setTokenURI] = useState(null)
@@ -24,6 +36,7 @@ export default function NFTPage( { location }) {
     const [tokenAuthor, setTokenAuthor] = useState(null)
     const [royaltyInfo, setRoyaltyInfo] = useState(null)
     const [readProvider, setReadProvider] = useReadProvider()
+    const [lastNFTId, setLastNFTId] = useState(null)
 
     const [contractError, setContractError] = useState(null);
 
@@ -48,9 +61,13 @@ export default function NFTPage( { location }) {
         if (!id || !readProvider) return;
 
         const contract = new ethers.Contract(contractAddress, contractABI, readProvider);
-        const author = await contract.authorOf(id);
-  
-        setTokenAuthor(author);
+        try {
+            const author = await contract.authorOf(id);
+            setTokenAuthor(author);
+        }
+        catch (e) {
+            setContractError(e);
+        }
     }
 
     const queryTokenData = async () => {
@@ -85,11 +102,34 @@ export default function NFTPage( { location }) {
         })
     }
 
+    const changeId = (right) => () => {
+        if (right) {
+            navigate('/nft?id=' + (parseInt(id) + 1));
+        }
+        else {
+            navigate('/nft?id=' + (parseInt(id) - 1));
+        }
+    }
+
     useEffect(() => {
         if (!id) {
             navigate('/');
         }
     }, [id])
+
+    useEffect(() => {
+        setContractError(null);
+    }, [id])
+
+    useEffect(async () => {
+        const contractAddress = config.contractAddresses.v1;
+        const contractABI = v1Abi;
+        const contract = new ethers.Contract(contractAddress, contractABI, readProvider);
+    
+        const newLastNFTId = (await contract.lastTokenId());
+    
+        setLastNFTId(newLastNFTId.toNumber());
+    }, [])
 
     useEffect(() => queryTokenURI(), [id, readProvider])
     useEffect(() => queryTokenData(), [tokenURI])
@@ -103,11 +143,15 @@ export default function NFTPage( { location }) {
             <Helmet>
                 <meta charSet="utf-8" />
                 <title>zang</title>
-                <meta name="icon"href="/public/favicon.ico" />
+                <meta name="icon" href="/public/favicon.ico" />
             </Helmet>
             <Header />
+            <div style={styles.arrowContainer}>
+                { id == 1 ? <></> : <a style={styles.arrow} className="icon" role="button" onClick={changeId(false)}>{'\u25c0'}</a>}
+                { lastNFTId && id == lastNFTId ? <></> : <a style={styles.arrow} className="icon" role="button" onClick={changeId(true)}>{'\u25b6'}</a> }
+            </div>
             <div className="columns m-4">
-                <div className="column is-half">
+                <div className="column">
                     { readProvider ? (
                         contractError ? <p>Could not retrieve contract info : {contractError.message}.</p> : (
                             <div>
@@ -124,15 +168,19 @@ export default function NFTPage( { location }) {
                     : <p>Connect a wallet to view this NFT</p>
                     }
                 </div>
-                <div className="column is-half">
-                <h1 className="title">{tokenData?.name || ''}</h1>
-                            <p className="subtitle">{tokenAuthor ? `by ${tokenAuthor}` : ''}</p>
-                            <p className="is-italic">{tokenData?.description || ''}</p>
-                            {royaltyInfo && tokenAuthor && royaltyInfo?.amount !== 0 ? 
-                            <p>{royaltyInfo.amount.toFixed(2)}% of every sale goes to {royaltyInfo.recipient == tokenAuthor ? 'the author' : royaltyInfo.recipient}.</p>
-                            : <></>
-                            }
+                { 
+                    contractError ? <></> : (
+                    <div className="column">
+                        <h1 className="title">{tokenData?.name || ''}</h1>
+                        <p className="subtitle">{tokenAuthor ? `by ${tokenAuthor}` : ''}</p>
+                        <p className="is-italic">{tokenData?.description || ''}</p>
+                        {royaltyInfo && tokenAuthor && royaltyInfo?.amount !== 0 ? 
+                        <p>{royaltyInfo.amount.toFixed(2)}% of every sale goes to {royaltyInfo.recipient == tokenAuthor ? 'the author' : royaltyInfo.recipient}.</p>
+                        : <></>
+                        }
                     </div>
+                    )
+                }
             </div>
         </div>
     )
