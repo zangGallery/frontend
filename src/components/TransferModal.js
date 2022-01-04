e29104dcc1741e247d777ec9f92d0def76eeee45
Samuele Marro <marrosamuele@gmail.com>
2022-01-04 21:16:13 +0100

    Renamed transfer modal title.

--- src/components/TransferModal.js
+++ src/components/TransferModal.js
@@ -38,7 +38,7 @@ export default function TransferModal ({ isOpen, setIsOpen, onClose, balance, av
         <div className="modal-background" onClick={() => closeModal(null, null)} />
         <div className="modal-card" style={styles.modalCard}>
             <header className="modal-card-head">
-            <p className="modal-card-title" style={styles.modalCardTitle}>List</p>
+            <p className="modal-card-title" style={styles.modalCardTitle}>Gift</p>
             </header>
             <section className="modal-card-body">
                 <p>Balance: {balance}</p>
