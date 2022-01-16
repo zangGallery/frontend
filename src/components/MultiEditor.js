61db3ae2e0e85917b0f14d4ac0cb8778a5b7057a
Samuele Marro <marrosamuele@gmail.com>
2022-01-16 20:48:53 +0100

    Added Markdown info.

--- src/components/MultiEditor.js
+++ src/components/MultiEditor.js
@@ -4,15 +4,33 @@ import rehypeSanitize from "rehype-sanitize";
 import MDEditor from "@uiw/react-md-editor"
 import { defaultCommands } from "../common/commands"
 
+const styles = {
+    link: {
+        textDecoration: 'underline'
+    },
+    plainEditor: {
+        fontFamily: 'monospace'
+    }
+}
+
 export default function MultiEditor({textType, value, setValue}) {
     return (
         <div>
             { textType == 'text/markdown' ? 
-                <MDEditor value={value} onChange={setValue} highlightEnable={false} previewOptions={{ rehypePlugins : [rehypeSanitize] }} commands={defaultCommands} /> :
-                <textarea className="textarea" value={value} onChange={(event) => setValue(event.target.value)} placeholder="Content of your artwork"></textarea>
+                (
+                    <div>
+                        <MDEditor value={value} onChange={setValue} highlightEnable={false} previewOptions={{ rehypePlugins : [rehypeSanitize] }} commands={defaultCommands} />
+                        <article className="message is-info is-small mt-2">
+                            <div class="message-body">
+                            <p>For help regarding Markdown, see the{' '}
+                            <a style={styles.link} target="_blank" rel="noopener noreferrer" href="https://docs.github.com/en/github/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax">GitHub Flavored Markdown guide</a>
+                            {' '}and the <a style={styles.link} target="_blank" rel="noopener noreferrer" href="https://github.github.com/gfm/">official spec</a>.</p>
+                            </div>
+                        </article>
+                    </div>
+                ):
+                <textarea style={styles.plainEditor} className="textarea" value={value} onChange={(event) => setValue(event.target.value)} placeholder="Content of your artwork"></textarea>
             }
-                
-            
         </div>
     )
 }
