160699c9152d0b1e61dd1c1a3c1ecc3bcc2ce4f7
Samuele Marro <marrosamuele@gmail.com>
2022-01-24 22:00:38 +0100

    Added type tag.

---
+++ src/components/TypeTag.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function TypeTag ({ type, isUri }) {
+    if (isUri) {
+        type = type.split(';')[0].split(':')[1];
+    }
+
+    if (type == 'text/plain') {
+        return <span class="tag is-info">plaintext</span>;
+    } else if (type == 'text/markdown') {
+        return <span class="tag is-link">markdown</span>;
+    } else {
+        return <></>;
+    }
+}
