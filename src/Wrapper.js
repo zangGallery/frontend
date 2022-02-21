78a13ca1689f85d71d6797c0a05c2cacdc645ae4
Samuele Marro <marrosamuele@gmail.com>
2022-02-21 18:22:01 +0100

    Improved page wrapping.

---
+++ src/Wrapper.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { useTransactionStatus } from "./common/transaction_status";
+
+export default function Wrapper ({ children, props }) {
+    const { transactions } = useTransactionStatus();
+    return (
+        <div>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <meta name="icon" href="/public/favicon.ico" />
+                <meta name="keywords" content="zang, text, NFTs, on-chain" />
+            </Helmet>
+            {
+                process.env.NODE_ENV !== 'development' ? (
+                    <Helmet>
+                        <meta http-equiv="Content-Security-Policy" content="script-src 'self'"/>
+                    </Helmet>
+                ) : <></>
+            }
+            {children}
+        </div>
+    )
+}
--- src/common/preprocess.js
+++ src/common/preprocess.js
@@ -1,25 +1,15 @@
 import React from "react";
-import { Helmet } from "react-helmet";
 import { RecoilRoot } from "recoil";
+import Wrapper from "../Wrapper";
 
 const wrapPageElement = ({ element, props }) => {
     return (
         <RecoilRoot {...props}>
-            <Helmet>
-                <meta charSet="utf-8" />
-                <meta name="icon" href="/public/favicon.ico" />
-                <meta name="keywords" content="zang, text, NFTs, on-chain" />
-                
-            </Helmet>
-            {
-                process.env.NODE_ENV !== 'development' ? (
-                    <Helmet>
-                        <meta http-equiv="Content-Security-Policy" content="script-src 'self'"/>
-                    </Helmet>
-                ) : <></>
-            }
-            {element}
-        </RecoilRoot>);
+            <Wrapper {...props}>
+                {element}
+            </Wrapper>
+        </RecoilRoot>
+    );
 }
 
 export {
