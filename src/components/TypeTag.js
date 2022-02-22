ef51a1f582eedf06175ae783f3aeab93999b7692
Samuele Marro <marrosamuele@gmail.com>
2022-02-22 17:40:15 +0100

    Fixed incorrect attribute in TypeTag.

--- src/components/TypeTag.js
+++ src/components/TypeTag.js
@@ -10,10 +10,10 @@ export default function TypeTag ({ type, isUri }) {
     }
 
     if (type == 'text/plain') {
-        return <span class="tag is-info">plaintext</span>;
+        return <span className="tag is-info">plaintext</span>;
     } else if (type == 'text/markdown') {
-        return <span class="tag is-link">markdown</span>;
+        return <span className="tag is-link">markdown</span>;
     } else {
-        return <span class="tag is-danger">unknown</span>;
+        return <span className="tag is-danger">unknown</span>;
     }
 }
