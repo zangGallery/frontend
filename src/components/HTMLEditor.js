fd596e24263a786b6d44aaa669c3a033d1ba7afc
Samuele Marro <marrosamuele@gmail.com>
2022-03-28 17:26:01 +0200

    Added SplitPane.

--- src/components/HTMLEditor.js
+++ src/components/HTMLEditor.js
@@ -1,5 +1,9 @@
 import React from "react";
+import { ReactDOM } from "react";
 import { useState } from "react";
+import SplitPane from "react-split-pane";
+import htmr from 'htmr';
+import sanitize from "sanitize-html";
 
 export default function HTMLEditor() {
     const [content, setContent] = useState('');
@@ -7,19 +11,31 @@ export default function HTMLEditor() {
         const AceEditor = require('react-ace').default;
         require('ace-builds/src-noconflict/mode-html');
         require('ace-builds/src-noconflict/theme-github');
-    
-        return (<AceEditor
-            mode="html"
-            theme="github"
-            onChange={setContent}
-            name="html-editor"
-            editorProps={{ $blockScrolling: true }}
-            setOptions={{
-            enableBasicAutocompletion: true,
-            enableLiveAutocompletion: true,
-            enableSnippets: true
-            }}
-        />);
+
+        return (
+            <div className="Resizer" style={{height: "500px"}}>
+                <SplitPane split="vertical" minSize={50} defaultSize={100}>
+                    <div width="200px">
+                        <AceEditor
+                            mode="html"
+                            theme="github"
+                            onChange={setContent}
+                            name="html-editor"
+                            editorProps={{ $blockScrolling: false }}
+                            setOptions={{
+                            enableBasicAutocompletion: true,
+                            enableLiveAutocompletion: true,
+                            enableSnippets: true
+                            }}
+                            style={{width: "100%"}}
+                        />
+                    </div>
+                    <div>
+                        {htmr(content)}
+                    </div>
+                </SplitPane>
+            </div>
+        )
       }
     return null;
 }
