ed7103ea0d7faf1ab0852aff2dd71c64b848ba30
Samuele Marro <marrosamuele@gmail.com>
2022-04-02 18:23:41 +0200

    Added listings querying for index.js.

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,18 +10,23 @@ import { Helmet } from "react-helmet";
 import { useRecoilState } from "recoil";
 import { formatError, standardErrorState } from "../common/error";
 import StandardErrorDisplay from "../components/StandardErrorDisplay";
+import { getListings } from "../common/listings";
 
 import "bulma/css/bulma.min.css";
 import "../styles/globals.css";
 
 export default function Home() {
+    const marketplaceAddress = config.contractAddresses.v1.marketplace;
+    const marketplaceABI = v1.marketplace;
+
     const [readProvider] = useReadProvider();
     const [lastNFTId, setLastNFTId] = useState(null);
     const [nfts, setNFTs] = useState([]);
+    const [listings, setListings] = useState({});
 
     const [, setStandardError] = useRecoilState(standardErrorState);
 
-    const increment = 5;
+    const defaultIncrement = 5;
 
     useEffect(async () => {
         const contractAddress = config.contractAddresses.v1.zang;
@@ -51,9 +56,44 @@ export default function Home() {
         }
 
         setNFTs(newNFTs);
+
+        return newNFTs;
     };
 
-    useEffect(() => getMoreIds(20), [lastNFTId]);
+    const queryListings = async (newNFTs) => {
+        if (!nfts || !readProvider) return;
+
+        const marketplaceContract = new ethers.Contract(
+            marketplaceAddress,
+            marketplaceABI,
+            readProvider
+        );
+
+        try {
+            const promiseIds = [];
+            const promises = [];
+            for (const nftId of newNFTs) {
+                if (!listings[nftId]) {
+                    promiseIds.push(nftId);
+                    promises.push(getListings(nftId, marketplaceContract));
+                }
+            }
+
+            const newListings = await Promise.all(promises);
+            const listingsObject = Object.fromEntries(promiseIds.map((promiseId, i) => [promiseId, newListings[i]]));
+
+            setListings((currentListings) => ({...currentListings, ...listingsObject}));
+        } catch (e) {
+            setStandardError(formatError(e));
+        }
+    };
+
+    const next = (increment) => {
+        const newNFTs = getMoreIds(increment);
+        queryListings(newNFTs);
+    }
+
+    useEffect(() => next(20), [lastNFTId]);
 
     return (
         <div>
@@ -67,7 +107,7 @@ export default function Home() {
                     <h1 className="title has-text-centered">Latest NFTs</h1>
                     <InfiniteScroll
                         dataLength={nfts.length}
-                        next={() => getMoreIds(increment)}
+                        next={() => next(defaultIncrement)}
                         hasMore={nfts.length < lastNFTId}
                         loader={<h4>Loading...</h4>}
                         endMessage={
