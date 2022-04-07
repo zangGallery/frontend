455d3d062e299ebabe8c64c97797365e1caef799
Samuele Marro <marrosamuele@gmail.com>
2022-04-07 19:03:12 +0200

    Added infobox for HTML editor.

--- src/components/HTMLEditor.js
+++ src/components/HTMLEditor.js
@@ -12,28 +12,35 @@ export default function HTMLEditor({value, setValue}) {
         require('ace-builds/src-noconflict/theme-monokai')
 
         return (
-            <div className="Resizer" style={{height: "500px"}}>
-                <Split vertical onSplitChanged={() => window.dispatchEvent(new Event('resize'))}>
-                    <div>
-                        <AceEditor
-                            mode="html"
-                            theme="monokai"
-                            onChange={setValue}
-                            name="html-editor"
-                            editorProps={{ $blockScrolling: false }}
-                            setOptions={{
-                            enableBasicAutocompletion: true,
-                            enableLiveAutocompletion: true,
-                            enableSnippets: true
-                            }}
-                            width="100%"
-                        />
+            <>
+                <div className="Resizer" style={{height: "500px"}}>
+                    <Split vertical onSplitChanged={() => window.dispatchEvent(new Event('resize'))}>
+                        <div>
+                            <AceEditor
+                                mode="html"
+                                theme="monokai"
+                                onChange={setValue}
+                                name="html-editor"
+                                editorProps={{ $blockScrolling: false }}
+                                setOptions={{
+                                enableBasicAutocompletion: true,
+                                enableLiveAutocompletion: true,
+                                enableSnippets: true
+                                }}
+                                width="100%"
+                            />
+                        </div>
+                        <div style={{height: '100%', overflow: 'scroll'}}>
+                            <HTMLViewer source={value}/>
+                        </div>
+                    </Split>
+                </div>
+                <article className="message is-info is-small mt-2">
+                    <div class="message-body">
+                        <p><strong>Note</strong>: Most HTML tags are supported, with the notable exception of <tt>{'<script>'}</tt>. A full list of the allowed tags will be released soon.</p>
                     </div>
-                    <div style={{height: '100%', overflow: 'scroll'}}>
-                        <HTMLViewer source={value}/>
-                    </div>
-                </Split>
-            </div>
+                </article>
+            </>
         )
       }
     return null;
